ae75be66f6cd9b3df3d9c066c36ef5ffca36c924
Remy Dinh <remy_dinh@yahoo.com>
2019-02-26 14:20:20 -0800

    Removing manual scripts now that the makefile is working!

--- scripts/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-g++ -I./include/ -Wall -std=c++14 ./src/main.cpp ./src/fxx/*/*.cpp -lsfml-graphics -lsfml-system -lsfml-window -o ./megaDan.out
--- scripts/manualLink.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-FXX="./src/fxx"
-
-g++ -Wall -std=c++14 ./src/obj/main.o $FXX/*/obj/*.o -o ./bin/megaDan.out -lsfml-window -lsfml-graphics -lsfml-system 
